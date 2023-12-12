b523a90f6ff9d96443da851dc53d6e29dbb3aaa2
Lucas Rezende <lucasffrezende@gmail.com>
2023-12-12 14:07:03 -0300

    Primeiro commit

---
+++ src/main/java/com/lucasffrezende/sorteadorweb/SorteadorWebApplication.java
@@ -0,0 +1,13 @@
+package com.lucasffrezende.sorteadorweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorteadorWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SorteadorWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasffrezende/sorteadorweb/SorteadorWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasffrezende.sorteadorweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorteadorWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
