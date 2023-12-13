0c25ab2034ce66adffc1ae6e8d86118e306136e4
Lucas Rezende <lucasffrezende@gmail.com>
2023-12-13 14:31:08 -0300

    Criação dos Repositories dos models programa, usuarioPrograma

---
+++ src/main/java/com/lucasffrezende/sorteadorweb/repositories/ProgramaRepository.java
@@ -0,0 +1,25 @@
+package com.lucasffrezende.sorteadorweb.repositories;
+
+import com.lucasffrezende.sorteadorweb.models.Programa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class ProgramaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional
+    public void salvar(Programa programa) {
+        em.merge(programa);
+    }
+
+    @Transactional
+    public void delete(Programa programa) {
+        em.merge(programa);
+    }
+
+}
---
+++ src/main/java/com/lucasffrezende/sorteadorweb/repositories/UsuarioProgramaRepository.java
@@ -0,0 +1,25 @@
+package com.lucasffrezende.sorteadorweb.repositories;
+
+import com.lucasffrezende.sorteadorweb.models.UsuarioPrograma;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class UsuarioProgramaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional
+    public void salvar(UsuarioPrograma usuarioPrograma) {
+        em.merge(usuarioPrograma);
+    }
+
+    @Transactional
+    public void delete(UsuarioPrograma usuarioPrograma) {
+        em.merge(usuarioPrograma);
+    }
+
+}
